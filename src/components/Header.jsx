438f36b0c5afec373b0a1bb800ff5f230785ec7b
unknown <ernestoaleman00@gmail.com>
2025-05-04 04:25:27 -0500

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Navigation';
+import './header.css'; 
+
+function Header() {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
