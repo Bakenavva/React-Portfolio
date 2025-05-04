c9af1e313c5d51ad37af3ad152ffd7c4f6d2fb20
unknown <ernestoaleman00@gmail.com>
2025-05-04 04:22:50 -0500

    Adding placeholder images

---
+++ src/assets/images/projects/index.js
@@ -0,0 +1,15 @@
+import placeholder1 from './placeholder1.png';
+import placeholder2 from './placeholder2.png';
+import placeholder3 from './placeholder3.png';
+import placeholder4 from './placeholder4.png';
+import placeholder5 from './placeholder5.png';
+import placeholder6 from './placeholder6.png';
+
+export default {
+  placeholder1,
+  placeholder2,
+  placeholder3,
+  placeholder4,
+  placeholder5,
+  placeholder6,
+};
