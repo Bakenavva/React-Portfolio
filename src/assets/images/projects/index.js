51d3c8f054f8255197aab40c7e84d42f0125cfd9
unknown <ernestoaleman00@gmail.com>
2025-05-04 19:04:50 -0500

    Configured import index with images

--- src/assets/images/projects/index.js
+++ src/assets/images/projects/index.js
@@ -1,19 +1,15 @@
-import placeholder1 from './placeholder1.png';
-import placeholder2 from './placeholder2.png';
-import placeholder3 from './placeholder3.png';
-import placeholder4 from './placeholder4.png';
-import placeholder5 from './placeholder5.png';
-import placeholder6 from './placeholder6.png';
+import KanbanBoardPreview from './KanbanBoardPreview.png';
+import BookSearchPreview from './BookSearchPreview.png';
+import EmployeeDatabasePreview from './EmployeeDatabasePreview.png';
+import CandidateSelectionPreview from './CandidateSelectionPreview.png';
+import WeatherDashPreview from './WeatherDashPreview.png';
+import CypressTestingPreview from './CypressTestingPreview.png';
 
 export default {
-  placeholder1,
-  placeholder2,
-  placeholder3,
-  placeholder4,
-  placeholder5,
-  placeholder6,
-};
-
-// Order:
-// Kanban, BookSearch, SQLEmpTracker, 
-// CandidateSearch, API Weather, Tech Quiz
+  KanbanBoardPreview,
+  BookSearchPreview,
+  EmployeeDatabasePreview,
+  CandidateSelectionPreview,
+  WeatherDashPreview,
+  CypressTestingPreview,
+}
